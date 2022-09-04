bf7e722c00f5f37124945bc528141ad26da43097
Jklo <jklo@itu.dk>
2022-09-04 15:18:52 +0200

    Created interface for leap year function.

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.Write("Hello World!");
+﻿using MyApp;
+
+var leapYear = new LeapYear();
+
+Console.Write("Enter a year of your choice and we'll tell you if it's a leap year!");
+Console.WriteLine();
+var year = Console.ReadLine();
+var yearInt = Int32.Parse(year);
+
+if (leapYear.isLeapYear(yearInt))
+{
+    Console.Write("Yay");
+}
+else
+{
+    Console.Write("Nay");
+}
